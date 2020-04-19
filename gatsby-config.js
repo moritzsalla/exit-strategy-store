5dcce50ec785dcf62d38125c21991e321c902b31
Moritz Salla <moritz.salla@hotmail.de>
2020-04-19 22:12:19 +0200

    minor

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv").config()
 module.exports = {
   siteMetadata: {
     title: `Exit Strategy`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `We are Forgetful Number, a collective of 38 photography students of the Royal Academy of Art, The Hague.`,
     author: `@exitstrategy`,
   },
   plugins: [
@@ -21,14 +21,31 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `white`,
+        theme_color: `white`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -41,7 +58,7 @@ module.exports = {
         apiVersion: "2020-04",
         verbose: true,
         paginationSize: 250,
-        includeCollections: ["shop"],
+        includeCollections: ["shop", "content"],
       },
     },
   ],
