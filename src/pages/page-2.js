878c182ea19701a5e20a810eb6cbb22082977f55
Moritz Salla <moritz.salla@hotmail.de>
2020-04-19 20:45:47 +0200

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = ({ data }) => {
+  const product = data.allShopifyProduct.edges
+
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      {product.map(i => (
+        <p key={i}>{i.node.vendor}</p>
+      ))}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query queryVendors {
+    allShopifyProduct {
+      edges {
+        node {
+          vendor
+        }
+      }
+    }
+  }
+`
 
 export default SecondPage
