0eb21f7c23d372a363b29dbbf7250207516483a3
Moritz Salla <moritz.salla@hotmail.de>
2020-04-29 17:34:57 +0200

    minor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import Image from './images/thumbnail.jpg';
 import styled from 'styled-components';
 
-import Thumbnail from './images/thumbnail.jpg';
-import Favicon from './images/thumbnail.jpg';
+import Image from './images/image.jpg';
 
 import 'reset-css';
 
@@ -26,38 +23,8 @@ const Text = styled.p`
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-
-        <title>Exit Strategies</title>
-        <meta name="title" content="Exit Strategies" />
-        <meta
-          name="description"
-          content="Forgetful Number presents Exit Strategies: a photography collective composed of the photography graduates at the Royal Academy of Art in The Hague. "
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://exitstrategies.store/" />
-        <meta property="og:title" content="Exit Strategies" />
-        <meta
-          property="og:description"
-          content="Forgetful Number presents Exit Strategies: a photography collective composed of the photography graduates at the Royal Academy of Art in The Hague. "
-        />
-        <meta property="og:image" content={Image} />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://exitstrategies.store/" />
-        <meta property="twitter:title" content="Exit Strategies" />
-        <meta
-          property="twitter:description"
-          content="Forgetful Number presents Exit Strategies: a photography collective composed of the photography graduates at the Royal Academy of Art in The Hague. "
-        />
-        <meta property="twitter:image" content={Image} />
-        <link rel="shortcut icon" href={Favicon} />
-      </Helmet>
-
       <Wrapper>
-        <img src={Thumbnail} height="200" width="auto" alt="logo"></img>
+        <img src={Image} height="200" width="auto" alt="logo"></img>
         <Title>Exit Strategies</Title>
         <Text>
           There's nothing here yet. Please check back within a couple of days!
