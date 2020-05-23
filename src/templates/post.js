57dc8f2a39481375f179e27af6d40bf55d2f0b64
Moritz Salla <moritz.salla@hotmail.de>
2020-05-23 14:14:04 +0200

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -86,7 +86,7 @@ export const query = graphql`
       images {
         localFile {
           childImageSharp {
-            resize(height: 1000, quality: 100) {
+            resize(height: 1000, quality: 90s) {
               src
             }
           }
